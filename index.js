ab37e274bd848989343b64675540b8007ab9ba6a
Matthew Denune <mattdenune@gmail.com>
2020-03-09 12:49:41 -0400

    added get and post routes

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express'),
     app = express();
+    bodyParser = require('body-parser');
+
+
+
+    var todoRoutes = require('./routes/todos')
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
     app.get('/', function(req, res){
-        res.json({message: "Hi there from express!"});
+        res.send("Hello from the ROOT route");
     })
 
-    app.get('/happy', function(req, res){
-        res.send(":)");
-    })
+    app.use('/api/todos', todoRoutes);
+
 
 app.listen(3000, function(){
     console.log("app is running on port 3000");
