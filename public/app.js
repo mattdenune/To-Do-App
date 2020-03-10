ebd67fe2eedf683fd52c2d0056469c91d61574d2
Matthew Denune <mattdenune@gmail.com>
2020-03-10 11:56:57 -0400

    created AJAX call and linked list of items to html list

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-alert("Hi from app.js")
+$(document).ready(function(){
+    $.getJSON('/api/todos')
+    .then(addTodos)
+    .catch(function(err){
+        alert(err);
+    })
+});
+
+function addTodos(todos){
+    //add todos to page here
+    todos.forEach(function(todo){
+        console.log(todo.name)
+        var newTodo = $('<li class="task">'+todo.name+'</li>')
+        $('.list').append(newTodo);
+    })
+}
