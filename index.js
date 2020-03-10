8694fc070dcc6d4d3a272f2fd49cf6c11903c8cb
Matthew Denune <mattdenune@gmail.com>
2020-03-10 10:47:22 -0400

    added views and public folders and linked to html and css

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ var express = require('express'),
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/views'));
+    
 
     app.get('/', function(req, res){
-        res.send("Hello from the ROOT route");
+        res.sendFile("index.html");
     })
 
     app.use('/api/todos', todoRoutes);
