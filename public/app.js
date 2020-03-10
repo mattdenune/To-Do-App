b9c5428af651e9556ac705afe9e521bf2aa35d1a
Matthew Denune <mattdenune@gmail.com>
2020-03-10 13:40:59 -0400

    added a functional delete button

--- public/app.js
+++ public/app.js
@@ -9,6 +9,10 @@ $(document).ready(function(){
         if (event.which == 13) {
             createTodo();
         }
+    });
+
+    $('.list').on('click', 'span', function(){
+        removeTodo($(this).parent())
     })
 });
 
@@ -21,7 +25,8 @@ function addTodos(todos){
 
 function addTodo(todo){
     console.log(todo.name)
-    var newTodo = $('<li class="task">' + todo.name +' <span>X</span> </li>');
+    var newTodo = $('<li class="task">' + todo.name +' <span>X</span></li>');
+    newTodo.data('id', todo._id);
     if (todo.completed) {
         newTodo.addClass("done");
     }
@@ -35,6 +40,21 @@ function createTodo(){
     .then(function(newTodo){
         $('#todoInput').val("");
         addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+function removeTodo(todo){
+    var clickedId = todo.data('id');
+    var deleteUrl = '/api/todos/' + clickedId;
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+    .then(function (data) {
+        todo.remove();
     })
     .catch(function(err){
         console.log(err);
